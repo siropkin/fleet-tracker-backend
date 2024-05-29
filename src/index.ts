d1ed63b9801b3929f78a9d7b7bc1ed1060fd2f50
Ivan Seredkin <ivan@netography.com>
2024-05-29 15:02:28 -0400

    Add Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
-import logger from "./utils/logger";
-import racesPaths from "./paths/races";
+import logger from './utils/logger';
+import racesPaths from './paths/races';
 
 const port = process.env.PORT ?? 5000;
 
 const app = new Elysia()
-    .use(logger)
-    .use(cors())
-    .get("/", (ctx) => {
-        ctx.log.info(ctx.request, "Request");
-        return { status: "ok" };
-    })
-    .group('api/v1/races', (app) => app.use(racesPaths))
-    .onError((ctx) => {
-        console.error(ctx.error);
-        if (ctx.code === 'NOT_FOUND') {
-            return '🤖 Route not found'
-        }
-    })
-    .listen(port);
+  .use(logger)
+  .use(cors())
+  .get('/', (ctx) => {
+    ctx.log.info(ctx.request, 'Request');
+    return { status: 'ok' };
+  })
+  .group('api/v1/races', (app) => app.use(racesPaths))
+  .onError((ctx) => {
+    console.error(ctx.error);
+    if (ctx.code === 'NOT_FOUND') {
+      return '🤖 Route not found';
+    }
+  })
+  .listen(port);
 
 console.log(`Server is running at ${app.server!.url}`);
--- src/paths/races.ts
+++ src/paths/races.ts
@@ -1,66 +1,66 @@
-import { Elysia } from "elysia";
-import parsePositions from "../utils/parsePositions";
+import { Elysia } from 'elysia';
+import parsePositions from '../utils/parsePositions';
 
 const races = new Elysia()
-    .get("/", (ctx) => {
-        ctx.log.debug(ctx.request, "Request");
-        return { status: "ok" };
-    })
-    .get("/:id/setup", async (ctx) => {
-        ctx.log.debug(ctx.request, "Request");
-        const url = `https://yb.tl/JSON/${ctx.params.id}/RaceSetup`;
-        ctx.log.debug({ url }, "Fetching YB");
-        const response = await fetch(url);
-        if (response.status !== 200) {
-            ctx.set.status = response.status;
-            throw new Error(response.statusText);
-        }
-        const body = await response.text();
-        const bodyJson = JSON.parse(body);
-        return {
-            id: bodyJson.url,
-            course: bodyJson.course,
-            teams: bodyJson.teams,
-            start: bodyJson.start,
-            stop: bodyJson.stop,
-            other: bodyJson,
-        };
-    })
-    .get("/:id/leaderboard", async (ctx) => {
-        ctx.log.debug(ctx.request, "Request");
-        const url = `https://yb.tl/JSON/${ctx.params.id}/leaderboard`;
-        ctx.log.debug({ url }, "Fetching YB");
-        const response = await fetch(url);
-        if (response.status !== 200) {
-            ctx.set.status = response.status;
-            throw new Error(response.statusText);
-        }
-        const body = await response.text();
-        return JSON.parse(body);
-    })
-    .get("/:id/teams-positions-all", async (ctx) => {
-        ctx.log.debug(ctx.request, "Request");
-        const url = `https://yb.tl/BIN/${ctx.params.id}/AllPositions3`;
-        ctx.log.debug({ url }, "Fetching YB");
-        const response = await fetch(url);
-        if (response.status !== 200) {
-            ctx.set.status = response.status;
-            throw new Error(response.statusText);
-        }
-        const body = await response.arrayBuffer();
-        return parsePositions(body);
-    })
-    .get("/:id/teams-positions-latest", async (ctx) => {
-        ctx.log.debug(ctx.request, "Request");
-        const url = `https://yb.tl/BIN/${ctx.params.id}/LatestPositions3`;
-        ctx.log.debug({ url }, "Fetching YB");
-        const response = await fetch(url);
-        if (response.status !== 200) {
-            ctx.set.status = response.status;
-            throw new Error(response.statusText);
-        }
-        const body = await response.arrayBuffer();
-        return parsePositions(body);
-    })
+  .get('/', (ctx) => {
+    ctx.log.debug(ctx.request, 'Request');
+    return { status: 'ok' };
+  })
+  .get('/:id/setup', async (ctx) => {
+    ctx.log.debug(ctx.request, 'Request');
+    const url = `https://yb.tl/JSON/${ctx.params.id}/RaceSetup`;
+    ctx.log.debug({ url }, 'Fetching YB');
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      ctx.set.status = response.status;
+      throw new Error(response.statusText);
+    }
+    const body = await response.text();
+    const bodyJson = JSON.parse(body);
+    return {
+      id: bodyJson.url,
+      course: bodyJson.course,
+      teams: bodyJson.teams,
+      start: bodyJson.start,
+      stop: bodyJson.stop,
+      other: bodyJson,
+    };
+  })
+  .get('/:id/leaderboard', async (ctx) => {
+    ctx.log.debug(ctx.request, 'Request');
+    const url = `https://yb.tl/JSON/${ctx.params.id}/leaderboard`;
+    ctx.log.debug({ url }, 'Fetching YB');
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      ctx.set.status = response.status;
+      throw new Error(response.statusText);
+    }
+    const body = await response.text();
+    return JSON.parse(body);
+  })
+  .get('/:id/teams-positions-all', async (ctx) => {
+    ctx.log.debug(ctx.request, 'Request');
+    const url = `https://yb.tl/BIN/${ctx.params.id}/AllPositions3`;
+    ctx.log.debug({ url }, 'Fetching YB');
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      ctx.set.status = response.status;
+      throw new Error(response.statusText);
+    }
+    const body = await response.arrayBuffer();
+    return parsePositions(body);
+  })
+  .get('/:id/teams-positions-latest', async (ctx) => {
+    ctx.log.debug(ctx.request, 'Request');
+    const url = `https://yb.tl/BIN/${ctx.params.id}/LatestPositions3`;
+    ctx.log.debug({ url }, 'Fetching YB');
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      ctx.set.status = response.status;
+      throw new Error(response.statusText);
+    }
+    const body = await response.arrayBuffer();
+    return parsePositions(body);
+  });
 
 export default races;
